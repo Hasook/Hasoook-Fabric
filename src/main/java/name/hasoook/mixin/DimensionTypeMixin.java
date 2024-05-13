922e17f39c864c97c126ab5721b8d4dedb8d35a4
Hasok <2656053560@qq.com>
2024-05-13 11:55:05 +0800

    mixin练习，让水可以倒在所有维度

---
+++ src/main/java/name/hasoook/mixin/DimensionTypeMixin.java
@@ -0,0 +1,18 @@
+package name.hasoook.mixin;
+
+import net.minecraft.world.dimension.DimensionType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(DimensionType.class) // 只有被 @Mixin 修饰的类才会被识别并注入
+public class DimensionTypeMixin {
+
+    /*@作者 TuRou
+    使水可以放置在所有维度上。*/
+
+    @Overwrite()
+    public boolean ultrawarm() {
+        return false; // 所有维度都返回 false, 包括地狱
+    }
+
+}
