0fd87fdbf9b6df315ca9081db8eff0c504a1dd0b
Hasok <2656053560@qq.com>
2024-05-29 02:09:08 +0800

    灵魂纠缠

--- src/main/java/com/hasoook/mixin/SoulEntanglementMixin.java
+++ src/main/java/com/hasoook/mixin/SoulEntanglementMixin.java
@@ -20,10 +20,10 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
-public abstract class LivingEntityMixin extends Entity {
+public abstract class SoulEntanglementMixin extends Entity {
     private Vec3d previousPos; // 存储实体位置
 
-    public LivingEntityMixin(EntityType<?> type, World world) {
+    public SoulEntanglementMixin(EntityType<?> type, World world) {
         super(type, world);
     }
 
