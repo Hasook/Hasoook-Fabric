4478a26efbf5192fa57209114775459d974fe715
Hasok <2656053560@qq.com>
2024-05-29 10:24:31 +0800

    创造标签

--- src/main/java/com/hasoook/item/ModItemGroups.java
+++ src/main/java/com/hasoook/item/ModItemGroups.java
@@ -13,15 +13,13 @@ import net.minecraft.util.Identifier;
 
 //模组创造物品选项卡
 public class ModItemGroups {
-    public static final ItemGroup RUBY_GROUP = Registry.register(Registries.ITEM_GROUP, new Identifier(Hasoook.MOD_ID, "ruby"),
+    public static final ItemGroup RUBY_GROUP = Registry.register(Registries.ITEM_GROUP, new Identifier(Hasoook.MOD_ID, "air_block"),
             FabricItemGroup.builder().displayName(Text.translatable("itemgroup.hasoook"))
                     .icon(() -> new ItemStack(ModItems.RUBY)).entries((displayContext, entries) -> {
                         entries.add(ModItems.RUBY); //模组物品
                         entries.add(Items.SUGAR); //原版物品
                         entries.add(ModBlocks.RUBY_BLOCK); //模组方块
-                        entries.add(ModBlocks.AIRBLOCK); //模组方块
-
-
+                        entries.add(ModBlocks.AIRBLOCK);
                     }).build());
 
     public static void registerItemGroup() {
