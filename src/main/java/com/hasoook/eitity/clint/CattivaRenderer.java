9901123c9e411d4ede429e7e0a7269de6985b677
Hasok <2656053560@qq.com>
2024-07-16 22:33:59 +0800

    调整了baby的尺寸

--- src/main/java/com/hasoook/eitity/clint/CattivaRenderer.java
+++ src/main/java/com/hasoook/eitity/clint/CattivaRenderer.java
@@ -23,7 +23,7 @@ public class CattivaRenderer extends GeoEntityRenderer<CattivaEntity> {
     public void render(CattivaEntity entity, float entityYaw, float partialTick, MatrixStack poseStack,
                        VertexConsumerProvider bufferSource, int packedLight) {
         if(entity.isBaby()) {
-            poseStack.scale(0.4f, 0.4f, 0.4f);
+            poseStack.scale(0.6f, 0.6f, 0.6f);
         }
 
         super.render(entity, entityYaw, partialTick, poseStack, bufferSource, packedLight);
