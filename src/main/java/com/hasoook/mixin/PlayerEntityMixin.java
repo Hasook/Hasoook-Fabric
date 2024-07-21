22bffaf012830be2554e67c7dfd744100a3e8ad6
Hasok <2656053560@qq.com>
2024-07-21 19:17:11 +0800

    绑定诅咒的物品不能丢弃

---
+++ src/main/java/com/hasoook/mixin/PlayerEntityMixin.java
@@ -0,0 +1,50 @@
+package com.hasoook.mixin;
+
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerEntity.class)
+public abstract class PlayerEntityMixin extends Entity {
+    public PlayerEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    @Final
+    @Shadow
+    private PlayerInventory inventory;
+
+    @Inject(method = "dropItem(Lnet/minecraft/item/ItemStack;ZZ)Lnet/minecraft/entity/ItemEntity;", at = @At("HEAD"), cancellable = true)
+    private void onDropItem(ItemStack itemStack, boolean throwRandomly, boolean retainOwnership, CallbackInfoReturnable<ItemEntity> cir) {
+        if (EnchantmentHelper.getLevel(Enchantments.BINDING_CURSE, itemStack) > 0) {
+            // 物品是否有绑定诅咒
+            cir.cancel();
+            cir.setReturnValue(null);
+            ItemStack Stack = itemStack.copy();
+            //取消丢弃然后复制一个物品
+
+            for (int slot = 0; slot < this.inventory.size(); ++slot) {
+                // 把物品放入玩家背包里的空槽位
+                ItemStack currentStack = this.inventory.getStack(slot);
+                if (currentStack.isEmpty()) {
+                    this.inventory.setStack(slot, Stack);
+                    break;
+                }
+            }
+        }
+    }
+
+}
+
