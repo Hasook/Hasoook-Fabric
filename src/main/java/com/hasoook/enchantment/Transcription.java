f00c6c2164d04298bd16579762986c1fd7ef385f
Hasok <2656053560@qq.com>
2024-07-21 21:27:55 +0800

    抄写

--- src/main/java/com/hasoook/enchantment/Transcription.java
+++ src/main/java/com/hasoook/enchantment/Transcription.java
@@ -11,8 +11,23 @@ public class Transcription extends Enchantment {
         super(rarity, target, slotTypes);
     }
 
+    public int getMinPower(int level) {
+        return 10 + 20 * (level - 1);
+    }
+
+    @Override
+    public int getMaxPower(int level) {
+        return super.getMinPower(level) + 50;
+    }
+
+    public boolean isTreasure() {
+        return true;
+        // 是否为宝藏附魔
+    }
+
     public boolean isAcceptableItem(ItemStack stack) {
         return stack.isOf(Items.WRITABLE_BOOK);
+        // 只有书与笔可以接受抄写附魔
     }
 
 }
