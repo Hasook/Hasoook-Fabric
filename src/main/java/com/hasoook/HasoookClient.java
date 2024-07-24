eacc01689e29843731487ca962e70ff8183cb293
Hasok <2656053560@qq.com>
2024-07-24 21:44:12 +0800

    删除了捣蛋猫

--- src/main/java/com/hasoook/HasoookClient.java
+++ src/main/java/com/hasoook/HasoookClient.java
@@ -2,7 +2,6 @@ package com.hasoook;
 
 import com.hasoook.commands.NbtCommand;
 import com.hasoook.eitity.ModEntities;
-import com.hasoook.eitity.clint.CattivaRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
@@ -10,7 +9,6 @@ import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 public class HasoookClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        EntityRendererRegistry.register(ModEntities.CATTIVA, CattivaRenderer::new);
         CommandRegistrationCallback.EVENT.register(
                 ((dispatcher, registryAccess, environment) -> NbtCommand.register(dispatcher))
         );
--- src/main/java/com/hasoook/item/ModItems.java
+++ src/main/java/com/hasoook/item/ModItems.java
@@ -6,6 +6,7 @@ import com.hasoook.item.custom.One_Hit_Obliterator;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.entity.EntityType;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.item.SpawnEggItem;
@@ -20,7 +21,7 @@ public class ModItems {
     public static final Item ONE_HIT_OBLITERATOR = registerItem("one_hit_obliterator", new One_Hit_Obliterator(new FabricItemSettings())); //必杀之剑
 
     public static final Item CATTIVA_SPAWN_EGG = registerItem("cattiva_spawn_egg",
-            new SpawnEggItem(ModEntities.CATTIVA, 0xffc5c9, 0x976069,
+            new SpawnEggItem(EntityType.CAT, 0xffc5c9, 0x976069,
                     new FabricItemSettings()));
 
     //创造物品选项卡
